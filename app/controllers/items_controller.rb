1c04ad6e25b68ec5af451f8cf477bb1b94b73b5e
GladysKigen <gladys.kigen@student.moringa.com>
2023-03-24 13:54:07 +0300

    completed with the outlined response

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items.as_json(include: { user: { only: [:id, :username, :city] } }, except: [:user_id, :created_at, :updated_at])
+        #render json: items,except: [:created_at, :updated_at], include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def show
+      user = User.find(params[:id])
+      if user
+        render json: user.as_json(include: { items: { only: [:id, :name, :description, :price] } }, except: [:created_at, :updated_at])
+      else
+        render_not_found_response
+      end
+
+      #render json: user,except: [:created_at, :updated_at],include: :items, status: :created 
+      #render json :user.item, except: [:created_at, :updated_at], status: :created
+    end
+  
+    private
+  
+    def render_not_found_response
+      render json: { error: "user not found" }, status: :not_found
+    end
+  
+  
+  
 end
